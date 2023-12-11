a2b94c010bbbc835155a9d31711f85524c8b452c
westkite05 <westkite23@naver.com>
2023-12-11 16:02:56 +0900

    first commit

---
+++ const/yourDetails.ts
@@ -0,0 +1,3 @@
+// Replace this with your contract address
+export const NFT_COLLECTION_ADDRESS =
+  "0x514C272ff7FD8f1Dd881517CC99bDF313C71CCB7"
---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react"
+import type { AppProps } from "next/app"
+import Head from "next/head"
+import ThirdwebGuideFooter from "../components/guide/ThirdwebGuideFooter"
+import "./styles/globals.css"
+
+// This is the chain your dApp will work on.
+const activeChain = "mumbai"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider
+      activeChain={activeChain}
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+    >
+      <Head>
+        <title>thirdweb Signature Based Minting</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="thirdweb Example Repository to Showcase signature based minting on an NFT Collection contract"
+        />
+        <meta name="keywords" content="thirdweb signature based minting" />
+      </Head>
+      <Component {...pageProps} />
+      <ThirdwebGuideFooter />
+    </ThirdwebProvider>
+  )
+}
+
+export default MyApp
